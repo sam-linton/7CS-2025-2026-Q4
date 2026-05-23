de4c7dd480fd8be8a689b602b9e90438efa70f9d
Samuel W Linton <sam.linton@harker.org>
2026-05-23 16:55:26 -0700

    Fixes to student submissions

---
+++ Prabagaran-Navin/script.js
@@ -0,0 +1,4 @@
+function showText() {
+    document.getElementById("message").textContent =
+        "Hello! The button was clicked!";
+}
---
+++ Tyagi-Aarin/script.js
@@ -0,0 +1,9 @@
+const bob_name = "Bob";
+const bob_age = 101200;
+const bob_height = 10.5;
+const likes_pizza = false;
+
+console.log("Name:" + bob_name);
+console.log("Age:" + bob_age);
+console.log("Height:" + bob_height);
+console.log("Likes pizza?????:" + likes_pizza);
